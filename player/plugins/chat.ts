63d15828b23f965258270018d206d69292998231
Dinislam Giniyatov <65029993+DarkCoder15@users.noreply.github.com>
2023-02-16 09:06:47 +0500

    move spammer.ts to chat.ts & more info in README

--- player/plugins/chat.ts
+++ player/plugins/chat.ts
@@ -1,6 +1,7 @@
 import { Module, VERSION } from "../../KiberKotleta";
 import Command from "../Command";
 import PacketEvent from "../KiberKotletaPacketEvent";
+import uuid from 'uuid';
 import { Player } from "../KiberKotletaPlayer";
 import moment from "moment";
 import { translateTextComponent } from "../../util/textComponent";
@@ -79,5 +80,38 @@ export default function chatPlugin(player: Player) {
 
     player.modules.push(TimestampsModule);
     player.modules.push(translateChatModule);
+
+    var i: NodeJS.Timer;
+    var spammed: number = 0;
+    player.commands.push(new Command(
+        "spam",
+        player.translate('cmd_spam_desc'),
+        player.translate('cmd_spam_usage'),
+        3,
+        (player: Player, args: string[]) => {
+            spammed = 0;
+            var count = parseInt(args[0]);
+            var interval = parseFloat(args[1]);
+            var message = args.slice(2).join(' ');
+            i = setInterval(() => {
+                spammed++;
+                if (spammed >= count) {
+                    return clearInterval(i);
+                }
+                player.targetClient.chat(message.replace(/%random%/g, uuid.v4().replace(/\-/g, '')));
+            }, interval * 1e3);
+        }
+    ));
+    player.commands.push(new Command(
+        "cancelspam",
+        player.translate('cmd_cancelspam_desc'),
+        "",
+        0,
+        (player: Player, args: string[]) => {
+            if (i) {
+                clearInterval(i);
+            }
+        }
+    ));
 
 }
